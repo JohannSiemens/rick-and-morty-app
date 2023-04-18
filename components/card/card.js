e4225296287101460f68a0111f977a41c313aff0
JohannSiemens <johann.siemens85@gmx.de>
2023-04-18 14:43:14 +0200

    created function for card as li-element in card.js

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,63 @@
-export function createCharacterCard() {}
+export function createCharacterCard(
+  characterName,
+  imageSource,
+  status,
+  type,
+  occurences
+) {
+  const card = document.createElement("li");
+  card.classList.add("card");
+  document.append(card);
+
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("card__image-container");
+  card.append(imageContainer);
+
+  const image = document.createElement("img");
+  image.classList.add("card__image");
+  image.setAttribute("src", imageSource);
+  image.setAttribute("alt", characterName);
+
+  const cardContent = document.createElement("div");
+  cardContent.classList.add("card__content");
+  card.append(cardContent);
+
+  const h2 = document.createElement("h2");
+  h2.classList.add("card__title");
+  h2.textContent = characterName;
+  cardContent.append(h2);
+
+  const descriptionList = document.createElement("dl");
+  descriptionList.classList.add("card__info");
+  cardContent.append(descriptionList);
+
+  const statusTitle = document.createElement("dt");
+  statusTitle.classList.add("card__info-title");
+  statusTitle.textContent = "Status";
+  descriptionList.append(statusTitle);
+
+  const statusValue = document.createElement("dd");
+  statusValue.classList.add("card__info-description");
+  statusValue.textContent = status;
+  descriptionList.append(statusValue);
+
+  const typeTitle = document.createElement("dt");
+  typeTitle.classList.add("card__info-title");
+  typeTitle.textContent = "Type";
+  descriptionList.append(typeTitle);
+
+  const typeValue = document.createElement("dd");
+  typeValue.classList.add("card__info-description");
+  typeValue.textContent = type;
+  descriptionList.append(typeValue);
+
+  const occurencesTitle = document.createElement("dt");
+  occurencesTitle.classList.add("card__info-title");
+  occurencesTitle.textContent = "Occurences";
+  descriptionList.append(occurencesTitle);
+
+  const occurencesValue = document.createElement("dd");
+  occurencesValue.classList.add("card__info-description");
+  occurencesValue.textContent = occurences;
+  descriptionList.append(occurencesValue);
+}
